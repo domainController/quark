3392e445af05e64fee562cdf420efc8943d669c8
kevin-garnette <noyau.imperatif@gmail.com>
2020-03-14 17:07:04 +0000

    beyond the google analytics interview

---
+++ test.php
@@ -0,0 +1,1 @@
+<?php echo 'LE LINK MARCHE'; ?>
---
+++ variable.php
@@ -0,0 +1,8 @@
+<?php
+// PHP Front to Back  [Part3] Variables, Constants and Data Types - Traversy Media
+
+	print 'Hello World';
+
+	$output = 'Hello Nerds!';
+	echo $output; 
+?>
